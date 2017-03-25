4092196e2f067c100a60d9183ee0ee2133955f00
Gorbunov Michael <miwagorbi@gmail.com>
2017-03-25 12:06:12 +0200

    Moved question enums to service.data

---
+++ src/main/java/com/voidaspect/triviadaemon/service/data/QuestionType.java
@@ -0,0 +1,36 @@
+package com.voidaspect.triviadaemon.service.data;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * @author mikhail.h
+ */
+@RequiredArgsConstructor
+@Getter
+public enum QuestionType {
+
+    MULTIPLE("multiple", "Multiple choice"),
+
+    BOOLEAN("boolean", "True or False");
+
+    private final String name;
+
+    private final String description;
+
+    public static Optional<QuestionType> getByName(String name) {
+        return Arrays.stream(values())
+                .filter(questionType -> questionType.name.equals(name))
+                .findAny();
+    }
+
+    public static Optional<QuestionType> getByDescription(String description) {
+        return Arrays.stream(values())
+                .filter(questionType -> questionType.description.equalsIgnoreCase(description))
+                .findAny();
+    }
+
+}
